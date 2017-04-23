5bd2f0f10c8799fcd6ce9e5a5d63acb288aa7e3e
Scintoth <Scott93Davis@gmail.com>
2017-04-23 01:22:11 +0100

    Pre-Merge commit

---
+++ Assets/Scripts/Units/Military/Actions/AttackAction.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GOAP;
+
+public class AttackAction : TargetedAction
+{
+    MilitaryUnit unit;
+
+    void SetupPreconditions()
+    {
+        AddPrecondition(new GOAPState("Target is Enemy", true));
+        AddPrecondition(new GOAPState("Target in Range", true));
+    }
+
+    void SetupSatisfactions()
+    {
+        satisfiesStates.Add(new GOAPState("Has Target", false));
+    }
+
+    public void SetUpEffects()
+    {
+        effects.Add(Attack);
+    }
+
+    void Attack()
+    {
+        unit.target.GetComponent<BaseUnit>().DealDamage(unit.damage);
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        unit = GetComponent <MilitaryUnit>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
